6b63992c1540cc121d2c79e987f3476c9b55981c
haniya-batool5 <bhaniya561@gmail.com>
2026-02-19 21:42:36 +0500

    Add files via upload

---
+++ bowl.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"     *	    *	    *          "<<endl;
+cout<<"      *       *       *         "<<endl;
+cout<<"       *       *       *        "<<endl;
+cout<<"      *       *       *         "<<endl;
+cout<<"     *       *       *          "<<endl;
+cout<<"      *       *       *         "<<endl;
+cout<<"       *       *       *        "<<endl;
+cout<<"********************************"<<endl;
+cout<<"*                              *"<<endl;
+cout<<" *                            * "<<endl;
+cout<<"  *                          *  "<<endl;
+cout<<"   *                        *   "<<endl;
+cout<<"    ************************    "<<endl;
+cout<<"   WELCOME TO FOODY APPLICATION "<<endl;
+return 0;
+}
