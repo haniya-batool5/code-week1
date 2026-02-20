01b71dab231dde8aa71713dd8ea89e4920db2713
haniya-batool5 <bhaniya561@gmail.com>
2026-02-20 11:43:22 +0500

    Add files via upload

---
+++ century.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+system("color 57");
+cout<<".....              ooooo    ooooo             ...........  "<<endl;
+cout<<"......            o     o  o     o            ..........   "<<endl;
+cout<<".......              ooo   o     o            .........    "<<endl;
+cout<<"........           oo      o     o            ........     "<<endl;
+cout<<".........         oooooooo  ooooo  th         .......      "<<endl;
+cout<<" .........       -----------------------      ......       "<<endl;
+cout<<"  .........          C E N T U R Y            .....        "<<endl;
+cout<<"    ........     -----------------------      ....         "<<endl;
+cout<<"        ....      ooooo ooooo o   o ooooo     ...          "<<endl;
+cout<<"            oo      o   o      o o    o       oo           "<<endl;
+cout<<"          __||__    o   oooo    o     o     __||__         "<<endl;
+cout<<"         |      |   o   o      o o    o    |      |        "<<endl;
+cout<<"---------|------|---o   ooooo o   o   o ---|------|--------"<<endl;
+return 0;
+}          
