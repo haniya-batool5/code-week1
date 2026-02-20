e95e6125ba427c455ea152a69a83685477d0d300
haniya-batool5 <bhaniya561@gmail.com>
2026-02-20 14:45:04 +0500

    Add files via upload

---
+++ diagram.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+system("color 87");
+cout<<"----------------------------   "<<endl;
+cout<<"       o  ^__^                 "<<endl;
+cout<<"        o (oo)\\_______        "<<endl<<endl;
+cout<<"          (__)\\       )\\/\\  "<<endl;
+cout<<"              ||----w  |       "<<endl<<endl;
+cout<<"              ||      ||       "<<endl;
+return 0; 
+}
