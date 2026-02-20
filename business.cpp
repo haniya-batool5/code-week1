97db2ea6af3c4390478178f508db1fa7a2f07383
haniya-batool5 <bhaniya561@gmail.com>
2026-02-20 17:36:59 +0500

    Add files via upload

---
+++ business.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<"             NUTRI SWEETS            "<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<"             MENU BOX                "<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<"   1. Cakes and cookies              "<<endl;
+cout<<"   2. Bread and milk                 "<<endl;
+cout<<"   3. Pastry                         "<<endl;
+cout<<"   4. Donuts                         "<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<" Please select your favorite item, enjoy your meal"<<endl;
+return 0;
+}
+     
---
+++ game.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  "<<endl;
+cout<<"        MY BELOVED FIGHTER           "<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  "<<endl;
+cout<<"        Player1 VS player2           "<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  "<<endl;
+cout<<"     ______         ______           "<<endl;
+cout<<"    | o  o |       | o  o |          "<<endl;
+cout<<"    --------       --------          "<<endl;
+cout<<"    |______|       |______|          "<<endl;
+cout<<"    (player1)      (player2)         "<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  "<<endl;
+cout<<"           Fight Started             "<<endl; 
+return 0;
+}
